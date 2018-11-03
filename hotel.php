b379702df5454a6c7c2f7caf918f5b9d086f79b7
Ion Batir <ion.batir@lateral-inc.com>
2018-11-03 13:19:22 +0200

    Add uuid generator

--- hotel.php
+++ hotel.php
@@ -6,6 +6,7 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
 include_once 'config/database.php';
+include_once 'config/core.php';
 include_once 'objects/hotel.php';
 
 $database = new Database();
