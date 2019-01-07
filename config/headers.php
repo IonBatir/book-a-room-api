5280dd65a8755f3caa1a1ad5a00279c136a95e4d
Ion Batîr <ionbatir@gmail.com>
2019-01-08 00:51:21 +0200

    Allow all origins

--- config/headers.php
+++ config/headers.php
@@ -1,5 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: book-a-room.ionbatir.com");
+    header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: *");
     header("Access-Control-Max-Age: 3600");
